ca2f1f79f11a37f283e4a71dad8799ca997b6df4
sola <145330217+solavrc@users.noreply.github.com>
2023-09-18 11:07:45 +0000

    Add ChatGPT for Discord BOT.

--- lib/chat_gpt-discord_bot-stack.ts
+++ lib/chat_gpt-discord_bot-stack.ts
@@ -1,6 +1,11 @@
 import {
+  RemovalPolicy,
   Stack,
   StackProps,
+  aws_ec2 as ec2,
+  aws_ecr_assets as ecra,
+  aws_ecs as ecs,
+  aws_logs as logs,
   aws_secretsmanager as secretsmanager,
 } from 'aws-cdk-lib'
 import { Construct } from 'constructs'
@@ -12,5 +17,52 @@ interface ChatGptDiscordBotStackProps extends StackProps {
 export class ChatGptDiscordBotStack extends Stack {
   constructor(scope: Construct, id: string, props: ChatGptDiscordBotStackProps) {
     super(scope, id, props)
+
+    const vpc = new ec2.Vpc(this, 'Vpc', {
+      subnetConfiguration: [{
+        name: 'Public',
+        subnetType: ec2.SubnetType.PUBLIC,
+        cidrMask: 20,
+      }, {
+        name: 'Isolated',
+        subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+        cidrMask: 20,
+      }],
+      maxAzs: 1,
+      natGateways: 0,
+    })
+
+    const cluster = new ecs.Cluster(this, 'Cluster', { vpc })
+    const taskDefinition = new ecs.TaskDefinition(this, 'TaskDefinition', {
+      compatibility: ecs.Compatibility.FARGATE,
+      cpu: '256',
+      memoryMiB: '512',
+      runtimePlatform: {
+        cpuArchitecture: ecs.CpuArchitecture.ARM64,
+        operatingSystemFamily: ecs.OperatingSystemFamily.LINUX,
+      },
+    })
+    taskDefinition.addContainer('Container', {
+      image: ecs.ContainerImage.fromAsset('./', {
+        platform: ecra.Platform.LINUX_ARM64,
+      }),
+      secrets: {
+        DISCORD_BOT_TOKEN: ecs.Secret.fromSecretsManager(props.secret, 'DISCORD_BOT_TOKEN'),
+        OPENAI_API_KEY: ecs.Secret.fromSecretsManager(props.secret, 'OPENAI_API_KEY'),
+      },
+      logging: ecs.LogDrivers.awsLogs({ streamPrefix: 'discord-bot', logGroup: new logs.LogGroup(this, 'LogGroup', { removalPolicy: RemovalPolicy.DESTROY }) }),
+    })
+    new ecs.FargateService(this, 'Service', {
+      assignPublicIp: true,
+      circuitBreaker: { rollback: true },
+      cluster,
+      desiredCount: 1,
+      enableExecuteCommand: false,
+      maxHealthyPercent: 100,
+      minHealthyPercent: 0,
+      platformVersion: ecs.FargatePlatformVersion.VERSION1_4,
+      taskDefinition,
+      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
+    })
   }
 }
---
+++ src/index.ts
@@ -0,0 +1,60 @@
+import { ChannelType, Client, Events, GatewayIntentBits } from 'discord.js'
+import { ChatCompletionMessageParam } from 'openai/resources/chat/completions'
+import { getChatCompletionMessage, trimMessagesToTokenLimit } from './chat_gpt'
+import OpenAI from 'openai'
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+})
+
+process.on('SIGTERM', () => process.exit(0));
+
+(async ({ DISCORD_BOT_TOKEN, OPENAI_API_KEY }) => {
+  const openai = new OpenAI({ apiKey: OPENAI_API_KEY })
+  client.on(Events.MessageCreate, async message => {
+    try {
+      if (message.mentions.has(client.user?.id!) && message.author.id !== client.user?.id) {
+        if (message.channel.isThread()) {
+          const threadMessages = await message.channel.messages.fetch()
+          const messages = await threadMessages.reverse().reduce<Promise<ChatCompletionMessageParam[]>>(async (messages, message) =>
+            [...await messages, await getChatCompletionMessage(client, message)], Promise.resolve([]))
+          const { object, id, model, choices, usage } = await openai.chat.completions.create({
+            model: 'gpt-4',
+            messages: trimMessagesToTokenLimit(messages, 4096),
+          })
+          console.log(JSON.stringify({ object, id, model, messages, choices, usage }))
+          await message.reply({ content: choices.at(0)?.message.content! })
+        } else if (message.channel.type === ChannelType.GuildText) {
+          const { cleanContent } = message
+          const messages: ChatCompletionMessageParam[] = [{ role: 'user', content: cleanContent }]
+          const { object, id, model, choices, usage } = await openai.chat.completions.create({
+            model: 'gpt-4',
+            messages,
+          })
+          console.log(JSON.stringify({ object, id, model, messages, choices, usage }))
+          const name = await openai.chat.completions.create({
+            model: 'gpt-3.5-turbo',
+            messages: [
+              { role: 'system', content: '概要を日本語100文字以内でタイトルとして抽出してください。' },
+              { role: 'user', content: cleanContent },
+              { role: 'assistant', content: choices.at(0)?.message.content! }
+            ],
+          }).then(({ object, id, model, choices, usage }) => {
+            console.log(JSON.stringify({ object, id, model, messages, choices, usage }))
+            return choices.at(0)?.message.content!
+          })
+          const thread = await message.channel.threads.create({ name, startMessage: message, autoArchiveDuration: 60 * 24 * 3 })
+          await thread.send({ content: choices.at(0)?.message.content! })
+        }
+      }
+    } catch (error) {
+      console.error(error)
+      await message.reply({ content: (error as Error).name })
+    }
+  })
+  await client.login(DISCORD_BOT_TOKEN)
+})(process.env)
