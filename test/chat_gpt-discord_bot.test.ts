f434f5e43f49c46bb3834e8577bcc538ef952315
sola <145330217+solavrc@users.noreply.github.com>
2023-09-18 09:42:02 +0000

    `npx cdk init --language=typescript`

---
+++ test/chat_gpt-discord_bot.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ChatGptDiscordBot from '../lib/chat_gpt-discord_bot-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/chat_gpt-discord_bot-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ChatGptDiscordBot.ChatGptDiscordBotStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
